ea8896c4d41d0813b1b0ecc5c7bbadbac04fcfb2
azimkhoja <ahmadkhonovazimkhoja@gmail.com>
2023-12-02 10:10:39 +0500

    client module changed

--- src/modules/clients/dto/create-client.dto.ts
+++ src/modules/clients/dto/create-client.dto.ts
@@ -1,73 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum, IsNotEmpty, IsString, Matches } from 'class-validator';
-enum Gender {
-  Male = 'male',
-  Female = 'female',
-}
+
 export class CreateClientDto {
   @ApiProperty({ example: 'Abdulloh', description: 'clients firstname ' })
-  @IsNotEmpty()
-  @IsString()
-  first_name: string;
+  name: string;
 
-  @ApiProperty({ example: 'Abdurahmonov', description: 'clients lastname ' })
-  @IsNotEmpty()
-  @IsString()
-  last_name: string;
-
-  @ApiProperty({ example: 'Abdurahim ugli', description: 'clients middlename' })
-  @IsString()
-  middle_name: string;
-
-  @ApiProperty({ example: '123456789', description: 'JShShIR' })
-  @IsNotEmpty()
-  @IsString()
-  @Matches(/^[0-9]{9}$/, { message: 'The tin must be a 9-digit numeric value' })
-  tin: string;
-
-  @IsEnum(Gender, {message: "Invalid gender entered"})
-  @ApiProperty({ example: 'male', description: 'gender of client' })
-  gender: string = 'male' || 'female';
-
-  @ApiProperty({ example: 'jismoniy', description: 'type of client' })
-  type: string = 'jismoniy' || 'yuridik';
-
-  @ApiProperty({
-    example: 'Namangan shahar islom karimov kochasi',
-    description: 'clients address ',
-  })
-  @IsString()
-  address: string;
-
-  @ApiProperty({
-    example: '+998 90 112 2442',
-    description: 'clients contact number ',
-  })
   @ApiProperty({ example: '+998 90 112 2442', description: 'mijoz tel raqami' })
   contact_number: string;
 
-  @ApiProperty({ example: '1978-08-13' })
-  date_of_birth: Date;
-  
-  
-  @Matches(/^[A-Z]{2}\d{7}$/, { message: 'Invalid passport number' })
-  @ApiProperty({ example: 'AD4586321' })
-  passport_seria: string;
-
-  @ApiProperty({ example: 'Namangan IIV' })
-  given_from: string;
-
-  @ApiProperty({ example: '1996-09-23' })
-  given_date: Date;
-
-  @ApiProperty({ example: '2006-08-13' })
-  untill_date: Date;
-
-  @ApiProperty({ example: 'Namangan viloyati Oxunboboyev' })
-  legal_address: string;
-
-  @ApiProperty({ example: "ro'yxatga olingan manzili" })
-  registered_address: string;
 
   @ApiProperty({ example: 'izoh' })
   description: string;
--- src/modules/clients/entities/client.entity.ts
+++ src/modules/clients/entities/client.entity.ts
@@ -1,53 +1,18 @@
-import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 import Model from '../../model/model.module';
+import { Payments } from 'src/modules/payments/entities/payment.entity';
 
 @Entity('Clients')
 export class Clients extends Model {
   @Column()
-  first_name: string;
+  name: string;
 
-  @Column()
-  last_name: string;
-
-  @Column()
-  middle_name: string;
-
-  @Column()
-  tin: string;
-
-  @Column({ enum: ['male', 'female'] })
-  gender: string;
-
-  @Column({ enum: ['jismoniy', 'yuridik'] })
-  type: string;
-
-  @Column()
-  address: string;
-
-  @Column()
-  contact_number: string;
-
-  @Column({ nullable: true })
-  date_of_birth: Date;
-
-  @Column({ nullable: false, unique: true })
-  passport_seria: string;
-
-  @Column({ nullable: false })
-  given_from: string;
-
-  @Column({ nullable: true })
-  given_date: Date;
-
-  @Column({ nullable: true })
-  untill_date: Date;
-
-  @Column({ nullable: true })
-  legal_address: string;
-
-  @Column({ nullable: true })
-  registered_address: string;
+  @Column({nullable: true})
+  phone_number: string;
 
   @Column({ nullable: true })
   description: string;
+
+  @OneToMany(() => Payments, payments => payments.clients)
+  payments: Payments[];
 }
